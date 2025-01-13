3d7c627927c572b0a9890b37e0817733b654d9c4
Allyson Ettinger <allysone@allenai.org>
2025-01-13 13:30:42 -0500

    add format rewriting

---
+++ run_inference.sh
@@ -0,0 +1,32 @@
+gantry run \
+  --workspace ai2/reddit \
+  --priority high \
+  --preemptible \
+  --beaker-image dirkg/OLMo \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --gpus 8 \
+  --replicas "${NUM_NODES}" \
+  --leader-selection \
+  --host-networking \
+  --budget ai2/oe-data \
+  --no-nfs \
+  --weka oe-training-default:/weka/oe-training-default \
+  --propagate-failure \
+  --propagate-preemption \
+  --synchronized-start-timeout 90m \
+  --no-python \
+  --env LOG_FILTER_TYPE=local_rank0_only \
+  --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env R2_PROFILE=R2 \
+  --env S3_PROFILE=S3 \
+  --env WEKA_PROFILE=WEKA \
+  --env-secret AWS_CONFIG=AWS_CONFIG \
+  --env-secret AWS_CREDENTIALS=AWS_CREDENTIALS \
+  --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
+  --env-secret WEKA_ENDPOINT_URL=WEKA_ENDPOINT_URL \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  --shared-memory 10GiB \
+  --yes \
+  --timeout=-1 \
+  -- python scripts/format-rewriting/qwen_inference.py
