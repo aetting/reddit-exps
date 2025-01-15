9f6069c04b1d26455d31242ca75dfb5115423520
Allyson Ettinger <allysone@allenai.org>
2025-01-15 10:53:43 -0500

    prompt templates

--- scripts/format-rewriting/prompt_templates.py
+++ scripts/format-rewriting/prompt_templates.py
@@ -1,19 +1,81 @@
+OPEN_ENDED ="""
+"{text}"
+
+First, judge whether this text has information relevant for academic learning, and record this as True or False.
+
+Take the educational information from the text and create multiple choice questions using that information. Use open-ended questions. Example:
+
+Question: How many legs do dogs have?
+A. 3
+B. 4
+C. 6
+D. 2
+
+Answer: B
+
+Give the answer as "A","B","C" or "D". DONOT include the letter (e.g. "A.") in the options. DONOT reference the text in the question.
+"""
+
 FILL_IN_BLANK ="""
 "{text}"
 
-Please take information from this text and create three multiple choice questions that use 2-4 blanks. Do not use any information that is not in the text. Example:
+First, judge whether this text has information relevant for academic learning, and record this as True or False.
 
-"Dogs are ___ with ___ legs and ___ tail."
+Take the educational information from the text and create multiple choice questions using that information. Use questions that contain 2-4 blanks. Example:
+
+Question: "Dogs are ___ with ___ legs and ___ tail."
 A. mammals; four; one
 B. mammals; two; one
 C. amphibians; two; one
 D. amphibians; four; two
 
 Answer: B
+
+Give the answer as "A","B","C" or "D". DONOT include the letter (e.g. "A.") in the options. DONOT reference the text in the question.
 """
 
-CHECK_ACADEMIC="""
+STATEMENT_COMPLETION ="""
 "{text}"
 
-Is the above text relevant for academic learning? Respond with "Yes" or "No".
+First, judge whether this text has information relevant for academic learning, and record this as True or False.
+
+Take the educational information from the text and create multiple choice questions using that information. Use questions that involve completing a statement. Example:
+
+Question: Dogs are mammals with
+A. Two legs and one tail
+B. Four legs and two tails.
+C. Four legs and one tail.
+D. One leg and four tails.
+
+Answer: C 
+
+
+Give the answer as "A","B","C" or "D". DONOT include the letter (e.g. "A.") in the options. DONOT reference the text in the question.
 """
+
+STATEMENT_TRUTH =""""{text}"
+
+First, judge whether this text has information relevant for academic learning, and record this as True or False.
+
+Take the educational information from the text and create a list of true statements and a list of false statements. Do not reference the text in the statements.
+"""
+
+OLD_TRUTH="""multiple choice questions using that information. Create questions that involve judging the truth of statements. The statements can be true or false.
+
+Example question: Statement 1 | Dogs are mammals. Statement 2 | Dogs have five legs.
+
+These should be the answer options: 
+A. True, True
+B. True, False
+C. False, True
+D. False, False
+
+Then give the answer based on the truth of the statements. For example, if both statements in the question are False, choose "D". If the first statement is True and the second statement is False, choose "B".
+
+
+DONOT include the letter (e.g. "A.") in the options. DONOT reference the text in the question.
+"""
+
+
+
+
--- scripts/format-rewriting/query_chatgpt.py
+++ scripts/format-rewriting/query_chatgpt.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import os
 from pydantic import BaseModel
 import prompt_templates
+import json
 
 from s3_functions import read_jsonl_from_s3
 
@@ -16,10 +17,16 @@ class MCQuestion(BaseModel):
     options: list[str]
     answer: str
 
-class Classification(BaseModel):
-    answer: str
+class QuestionList(BaseModel):
+    academic_relevance: bool
+    questions: list[MCQuestion]
 
-def get_responses(prompts, model="gpt-4o-mini", temperature=0.7, max_tokens=150):
+class TrueFalse(BaseModel):
+    true: list[str]
+    false: list[str]
+    academic_relevance: bool
+
+def get_responses(prompts, model="gpt-4o-mini", temperature=0.7, max_tokens=500):
     """
     Takes a list of prompts and submits them to the OpenAI API.
     Returns responses for each prompt.
@@ -54,7 +61,7 @@ def get_responses(prompts, model="gpt-4o-mini", temperature=0.7, max_tokens=150)
             responses[prompt] = f"Error: {str(e)}"
     return responses
 
-def get_responses_structured(prompts, structure, model="gpt-4o-mini", temperature=0.7, max_tokens=150):
+def get_responses_structured(prompts, structure, model="gpt-4o-mini", temperature=0.7, max_tokens=800):
     """
     Takes a list of prompts and submits them to the OpenAI API.
     Returns responses for each prompt.
@@ -91,20 +98,49 @@ def get_responses_structured(prompts, structure, model="gpt-4o-mini", temperatur
 
 # Example usage
 if __name__ == "__main__":
-    prompts = []
-    loc = "s3://ai2-llm/pretraining-data/sources/reddit/dolma_raw/merged_versions/merged_qa_wsubreddit/merged_qa_prefilter/merged_qa_prefilter_densesubs_highthresh/documents/"
-    file_keys = [
-        os.path.join(loc,"merged_qa_prefilter_densesubs_highthresh-0000.json.gz")
-        ] 
-
-    template = prompt_templates.CHECK_ACADEMIC
-    for i,line in enumerate(read_jsonl_from_s3(file_keys)):
-        prompts.append(template.format(text=line["text"]))
-        if i >= 30: break
+    # prompts = []
+    # loc = "s3://ai2-llm/pretraining-data/sources/reddit/dolma_raw/merged_versions/merged_qa_wsubreddit/merged_qa_prefilter/merged_qa_prefilter_densesubs_highthresh/documents/"
+    # file_keys = [
+    #     os.path.join(loc,"merged_qa_prefilter_densesubs_highthresh-0000.json.gz")
+    #     ] 
     
-    results = get_responses_structured(prompts,Classification)
+    # for i,line in enumerate(read_jsonl_from_s3(file_keys)):
+    #     prompts.append(template.format(text=line["text"]))
+    #     if i >= 30: break
+    
+    
+    prompts = [
+        "If you could only eat cuisine from 2 countries, which ones would you choose?\n\nItalian and Indian.",
+        """ELI5: How does the socialism in Venezuela differ from Democratic Socialism?
+
+First of you have to remember that socialism, democratic socialism, social democracies, etc. describe ideologies and streams of thoughts about what constitutes good policies and governments. They are not however manuals that you have to follow 100%.
+
+Just like two countries with market economies are not the same, so do socialist countries.
+
+With that said, Venezuela is not a democracy, even though they might like to say that, it's pretty clear that they are pretty close to a dictatorship.
+
+Democratic socialism or social democracies are normal democracies, they do not have a drastically different from of government (e.g. Germany, Sweden, France, etc. are social democracies).
+
+However they have a specific view on the role of government in the economy and how to improve the lives of all citizens.
+
+In contrast to capitalism both Socialism and DemSoc believe in stronger regulation and taxes, in welfare programs and in strengthening workers opposed to owners.
+
+A big difference between the two philosophies is that Socialism believes in a planned economy, where the government prescribes prices, production capacities, etc. basically tightly controlling the whole market place.
+
+DemSocs "merely" believe that a free market needs to be regulated in a way that everybody profits. This means more things like minimum wages, welfare programs, etc. but no direct interference in how companies do their business."""
+    ]
+
+    template = prompt_templates.STATEMENT_TRUTH
+    prompts = [template.format(text=p,num="two") for p in prompts]
+    
+    results = get_responses_structured(prompts,TrueFalse)
     for prompt, response in results.items():
-        print(f"Prompt: {prompt}\nResponse: {response}\n")
+        # print(f"Prompt: {prompt}")
+        d = json.loads(response)
+        # for q in d["questions"]:
+        #     print(f"Question: {q}\n")
+        print(d)
+        print("\n~~~~~~\n")
 
 
 
