84b764234bad342b404908c6fe6049a5d2c85470
Allyson Ettinger <allysone@allenai.org>
2025-07-08 14:24:37 -0400

    update run script

--- format-rewriting/run_get_prompts_from_dolma_filelist.sh
+++ format-rewriting/run_get_prompts_from_dolma_filelist.sh
@@ -1,4 +1,5 @@
 python get_prompts_from_dolma_filelist.py \
     --num_processes 5 \
-    --outdir /home/ec2-user/prompt_script_test_3 \
+    --input_dir s3://ai2-llm/pretraining-data/sources/reddit/dolma_raw/merged_versions/merged_qa_wsubreddit/merged_qa_prefilter/merged_qa_prefilter_densesubs_lowthresh/documents/ \
+    --outdir /home/ec2-user/prompt_script_test \
     --model gpt-4o-mini
